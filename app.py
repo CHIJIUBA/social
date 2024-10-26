b2b33f5d7cf84cfc6c620641c925e9d443b79a25
CHIJIUBA <chijiubaonyedikachukwu@gmail.com>
2024-10-26 12:24:46 +0100

    go

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request
+import json
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Hello world", 200
+
+
+@app.route("/user")
+def user():
+    name = request.json.get("name")
+    return "Hello {}".format(name), 200
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
